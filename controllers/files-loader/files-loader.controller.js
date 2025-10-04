f423e9a0f44abb5f7da19d05c92d8216348bfbcf
luricisme <trinhnguyenluong182306@gmail.com>
2025-10-05 01:16:43 +0700

    Split the text into chunks

--- controllers/files-loader/files-loader.controller.js
+++ controllers/files-loader/files-loader.controller.js
@@ -1,6 +1,5 @@
 import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
-import axios from 'axios';
-import fs from 'fs';
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 class FilesLoaderController {
     async pdfLoader(req, res) {
@@ -14,19 +13,36 @@ class FilesLoaderController {
                 });
             }
 
-            // Fix get direct PDF file URL: https://support.cloudinary.com/hc/en-us/articles/360016480179-Why-are-PDF-or-ZIP-files-appearing-in-the-Media-Library-but-their-download-URLs-return-an-error
+            // Fix get direct PDF file URL: 
+            // https://support.cloudinary.com/hc/en-us/articles/360016480179-Why-are-PDF-or-ZIP-files-appearing-in-the-Media-Library-but-their-download-URLs-return-an-error
             const response = await fetch(pdfUrl);
             const arrayBuffer = await response.arrayBuffer();
             const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
             const loader = new WebPDFLoader(blob);
             const docs = await loader.load();
 
-            
+            let pdfTextContent = '';
+
+            docs.forEach(doc => {
+                pdfTextContent = pdfTextContent + doc.pageContent;
+            });
+
+            // 2. Split the text into small chunks
+            const splitter = new RecursiveCharacterTextSplitter({
+                chunkSize: 100,
+                chunkOverlap: 20,
+            });
+            const output = await splitter.createDocuments([pdfTextContent]);
+
+            let splitterList = [];
+            output.forEach(doc => {
+                splitterList.push(doc.pageContent)
+            })
 
             return res.status(200).json({
                 success: true,
                 message: "Content in PDF File",
-                results: docs,
+                results: splitterList,
             });
 
         } catch (error) {
